538518ad06c04aa03651303b93d776532e27783c
Demba Siby <dembasiby@gmail.com>
2017-03-24 15:40:52 +0000

    Created 'deaf_grandma' file and filled it with the content of the dialogue.

---
+++ 07-flow_control/deaf_grandma.rb
@@ -0,0 +1,9 @@
+def deaf_grandma
+
+  what_you_said_to_grandma = gets.chomp
+  puts "HUH!  SPEAK  UP,  SONNY!"
+  year = rand(1930..1950)
+  puts "NO, NOT SINCE #{year}!"
+
+  #write the logic of the method
+end
