f631fe82fa4dba75a5c6a37c40bc144d796121e4
Demba Siby <dembasiby@gmail.com>
2017-03-26 17:52:36 +0000

    Completed the extended version of the exercise

--- 07-flow_control/deaf_grandma.rb
+++ 07-flow_control/deaf_grandma.rb
@@ -1,14 +1,15 @@
 def deaf_grandma
   puts "Grandma: What do you say Sonny?"
+  count_bye = 0
 
   while true
     what_you_said_to_grandma = gets.chomp
 
     if what_you_said_to_grandma.upcase != what_you_said_to_grandma
       puts "HUH!  SPEAK  UP,  SONNY!"
-      puts "BYE, SONNY!"
     elsif what_you_said_to_grandma == 'BYE'
-      break
+      count_bye += 1
+      break if count_bye >= 3
     else
       year = rand(1930..1950)
       puts "NO, NOT SINCE #{year}!"
